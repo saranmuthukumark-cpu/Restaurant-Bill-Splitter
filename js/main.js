60b027f2db5c919db11c1da7a84e4bf8113c1521
saranmuthukumark-cpu <saranmuthukumar.k@gmail.com>
2026-02-05 15:15:51 +0530

    All Functionalities added

---
+++ js/main.js
@@ -0,0 +1,92 @@
+
+
+function updateTax(){
+
+const tax = document.getElementById('tax').value;
+document.getElementById('tax-label').textContent= tax +"%";
+calculate();
+}
+
+function updateTip(){
+const tip =  document.getElementById('tip').value;
+document.getElementById('tip-label').textContent = tip + "%";
+calculate();
+}
+
+
+// 
+
+function increasePeople(){
+
+const peopleInput = document.getElementById('people');
+
+peopleInput.value = parseInt(peopleInput.value) + 1;
+
+calculate();
+}
+
+
+//  
+
+function decreasePeople(){
+const peopleInput = document.getElementById('people');
+if(parseInt(peopleInput.value)>1){
+peopleInput.value = parseInt(peopleInput.value) -1 ;
+calculate();
+}
+}
+
+
+// 
+
+function resetCalculate(){
+
+document.getElementById('bill').value ="";
+document.getElementById('tax').value = 8;
+document.getElementById('tip').value = 10 ;
+document.getElementById('people').value = 2;
+document.getElementById('tax-label').textContent = "8%";
+document.getElementById('tip-label').textContent = "10%";
+
+calculate();
+}
+
+
+//  
+
+
+function calculate(){
+
+
+const bill = parseInt( document.getElementById('bill').value) || 0;
+const tax = parseFloat( document.getElementById('tax').value) || 0;
+const tip = parseFloat( document.getElementById('tip').value) || 0;
+const people = parseFloat( document.getElementById('people').value) || 1;
+
+ 
+const taxAmount = (bill* tax)/100;
+
+const tipAmount = (bill * tip )/100;
+
+const total = bill + taxAmount + tipAmount;
+
+const perPerson = total/people;
+
+document.getElementById('count').textContent =people;
+document.getElementById('taxAmount').textContent = `₹${taxAmount.toFixed(2)}`;
+document.getElementById('tipAmount').textContent = `₹${tipAmount.toFixed(2)}`;
+document.getElementById('sub-total').textContent = `₹${bill.toFixed(2)}`;
+document.getElementById('total').textContent = `₹${total.toFixed(2)}`;
+document.getElementById('perPerson').textContent = `₹   ${perPerson.toFixed(2)}`;
+
+
+
+}
+
+calculate();
+
+
+
+
+
+
